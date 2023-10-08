113c26bca6bf5b90b52ebd06df2cc84d5eaa97c5
Lê Tấn Đạt <80414799+tandatle058@users.noreply.github.com>
2023-10-08 09:04:09 +0700

    bug -new folder

---
+++ src/Demeter.Acount/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace demeter.acount.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
