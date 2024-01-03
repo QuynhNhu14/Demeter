1e5b07aff786b05a176ea495c3bb16e84467badb
QuynhNhu14 <quynhnhu.9a3.2016@gmail.com>
2024-01-03 21:30:09 +0700

    mapping

--- src/Demeter.Core/Extensions/AutoMapperProfile.cs
+++ src/Demeter.Core/Extensions/AutoMapperProfile.cs
@@ -9,11 +9,27 @@ public class AutoMapperProfile : Profile
     {
         CreateMap<Domain.AppSettings, Entities.AppSettings>();
         CreateMap<Entities.AppSettings, Domain.AppSettings>();
+
         CreateMap<Domain.Users, Entities.Users>()
             .ForMember(dest => dest.AddressJson, act => act.MapFrom(src => JsonConvert.SerializeObject(src.Address)));
         CreateMap<Entities.Users, Domain.Users>()
             .ForMember(dest => dest.Address, act => act.MapFrom(src => JsonConvert.DeserializeObject<Domain.Address>(src.AddressJson)));
         CreateMap<Domain.Account, Entities.Account>();
         CreateMap<Entities.Account, Domain.Account>();
+
+        CreateMap<Domain.Orders, Entities.Orders>();
+        CreateMap<Entities.Orders, Domain.Orders>();
+        CreateMap<Domain.OrderItem, Entities.OrderItem>();
+        CreateMap<Entities.OrderItem, Domain.OrderItem>();
+        CreateMap<Domain.Voucher, Entities.Voucher>();
+        CreateMap<Entities.Voucher, Domain.Voucher>();
+        
+        CreateMap<Domain.Products, Entities.Products>();
+        CreateMap<Entities.Products, Domain.Products>();
+        CreateMap<Domain.Category, Entities.Category>();
+        CreateMap<Entities.Category, Domain.Category>();
+        CreateMap<Domain.Prices, Entities.Prices>();
+        CreateMap<Entities.Prices, Domain.Prices>();
+        
     }
 }
