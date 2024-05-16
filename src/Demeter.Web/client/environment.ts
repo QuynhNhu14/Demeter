bcde3788f446a489dcdf355d32dd3ed190c4a4b3
QuynhNhu14 <quynhnhu.9a3.2016@gmail.com>
2024-05-16 07:44:49 +0700

    Change env

--- src/Demeter.Web/client/environment.ts
+++ src/Demeter.Web/client/environment.ts
@@ -3,19 +3,19 @@
 interface EnvironmentConfig {
     apiUrl: string;
     port: string;
-    vercelUrl: string;
+    DEMETERUrl: string;
   }
   
   const getEnvironmentConfig = (): EnvironmentConfig => {
     // Environment-specific logic
     const defaultPort = '5029'; // Default port if not specified
-    const apiUrl = import.meta.env.VERCEL_URL ? `https://${import.meta.env.VERCEL_URL}` : `http://localhost:${defaultPort}`;
+    const apiUrl = import.meta.env.DEMETER_URL ? `https://${import.meta.env.DEMETER_URL}` : `http://localhost:${defaultPort}`;
 
   
     return {
       apiUrl: `${apiUrl}/api`,
       port: import.meta.env.PORT as string ?? defaultPort,
-      vercelUrl: import.meta.env.VERCEL_URL as string ?? 'localhost'
+      DEMETERUrl: import.meta.env.DEMETER_URL as string ?? 'localhost'
     };
   };
   
